7a908db60bde7165d0cf4799fc06e505a2788224
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-09-22 11:17:22 -0500

    First commit

---
+++ ClassesExercise1/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise1
+{
+    public class Car
+    {
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Price { get; set; }
+
+    }
+}
---
+++ ClassesExercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClassesExercise1;
+
+var car1 = new Car();
+
+
+car1.Type = "Mercedes";
+car1.Model = "Mercedes-Meybach GLS 600 SUV";
+car1.Year = "2022";
+car1.Price = "Starting at $165,100";
+
+Console.WriteLine($"Brand: {car1.Type}");
+Console.WriteLine($"Model: {car1.Model}");
+Console.WriteLine($"Year: {car1.Year}");
+Console.WriteLine($"Price: {car1.Price}");
+
+
+
+//var Type = car1.Brand = "Mercedes";
+//var model = car1.Model = "Mercedes-Meybach GLS 600 SUV";
+//var year = car1.Year = "2022";
+//var price = car1.Price = "Starting at $165,100";
+
+//Console.WriteLine($"Brand: {Type}");
+//Console.WriteLine($"Model: {model}");
+//Console.WriteLine($"Year: {year}");
+//Console.WriteLine($"Price: {price}");
